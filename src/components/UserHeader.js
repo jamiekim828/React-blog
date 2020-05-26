a78849b3173c3ff4341f9d61e5f235d5ae2210d2
jamiekim828 <jamiekim828@gmail.com>
2020-05-26 19:32:47 +0200

    solve overfetching issue with memoize

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
   render() {
-    return <div>UH</div>;
+    // we can move this to mapStateToProps
+    // const user = this.props.users.find((user) => user.id === this.props.userId);
+    // now we need only this.props
+
+    const { user } = this.props;
+
+    if (!user) {
+      return null;
+    }
+    return <div className='header'>{user.name}</div>;
   }
 }
 
-export default UserHeader;
+// get user as props
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find((user) => user.id === ownProps.userId) };
+};
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
